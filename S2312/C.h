b9d18420185206a22f56600a5ffb8fd773607394
NoRax <norax@qq.com>
2017-09-13 17:38:49 +0800

    new test fix

---
+++ S2312/C.h
@@ -0,0 +1,14 @@
+//
+//  C.h
+//  S2312
+//
+//  Created by NoRax on 2017/9/12.
+//  Copyright © 2017年 ZITTX. All rights reserved.
+//
+
+#ifndef C_h
+#define C_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* C_h */
